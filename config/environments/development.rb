2078b53bc9a7c273c716d008362463a1e2464f9f
Tamer Shlash <mr.tamershlash@gmail.com>
2016-09-04 08:08:41 +0800

    added redis initializer, and environment variable to test and development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  ENV['REDIS_URL'] = 'redis://127.0.0.1:6379/0'
 end
