7554153f2b7c9eff67d0c9317520d25e31f8f5fa
Tamer Shlash <mr.tamershlash@gmail.com>
2016-09-05 16:32:58 +0800

    clean database and redis before each test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Reset database after each test
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.around(:each) do |test|
+    DatabaseCleaner.cleaning { test.run }
+  end
+
+  config.before(:each) do
+    REDIS.flushdb
+  end
 end
